6dceed28d93cf644dbe481a9a13f1605c990c818
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-10-06 15:16:22 -0600

    modified job to take parameters and start evaluate_network.py fixed spacing in other files

--- Network Evaluation Examples/job.sh
+++ Network Evaluation Examples/job.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #SBATCH --time=24:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=8   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=32768M   # memory per CPU core
 #SBATCH --mail-user=michael.bradshawiii@colorado.edu   # email address
 #SBATCH --mail-type=FAIL
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-which python3 > delete.txt
-~/python3/bin/python3 michaels_test.py > output.txt 
+# arg 1 is the edge list for the network
+# arg 2 is the disease gene sets
+# arg 3 is the output dir
+
+~/python3/bin/python3 evaluate_network.py $1 $2 $3 > $3/output.txt
